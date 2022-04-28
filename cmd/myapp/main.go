3013ae4b71c6ac0107544482b7d7159b3ef2dc98
Евгений <javany25@mail.ru>
2022-04-28 11:20:34 +0300

    Initial commit

---
+++ cmd/myapp/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/LopatkinEvgeniy/go-pkg-example/internal/mylib"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var a int64
+	var b int64
+
+	cmd := &cobra.Command{
+		Use:   "test",
+		Short: "my test program",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("sum of %d and %d is %d\n", a, b, mylib.Add(a, b))
+		},
+	}
+
+	cmd.Flags().Int64Var(&a, "a", 0, "first number")
+	cmd.Flags().Int64Var(&b, "b", 0, "second number")
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
---
+++ internal/mylib/add.go
@@ -0,0 +1,6 @@
+package mylib
+
+// Add returns a sum of two integers.
+func Add(a int64, b int64) int64 {
+	return a + b
+}
